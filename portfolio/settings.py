5b7d36cf08b81096d91997b5ca548c7e96f960c6
Tristan Jahnke <trjahnke@protonmail.com>
2022-02-25 17:33:36 -0600

    update allowed hosts for root domain

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -33,7 +33,7 @@ if os.environ['DJANGO_DEBUG'] == 'True':
 else:
     DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', '.herokuapp.com']
+ALLOWED_HOSTS = ['tristanjahnke.com', '.herokuapp.com']
 
 
 # Application definition
