f2825ef2862ff0e47ba8603bb2777c4d5f3eb572
Tristan Jahnke <trjahnke@protonmail.com>
2022-02-25 16:16:04 -0600

    test

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -131,10 +131,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-# STATICFILES_DIRS = (
-#     os.path.join(BASE_DIR, 'portfolio', 'static'),
-# )
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 SITE_ID = 1
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
